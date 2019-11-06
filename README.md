6f43a800efbe29584a0b9b9d6f76208cdd1a2abe
Michael Ireland <mikeofireland@gmail.com>
2019-11-06 10:43:53 -0800

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+# win-cert-gen
+> Generates self-signed certs through powershell and adds them to the current users windows certificate store.  For local development testing only.
+
+## Security Warnings
+* __Use at your own risk__
+* __Keep expiration as short as possible__
+* __Clear out unused root certs as they are a user/machine vulnerability__
+
+## Requirements
+* powershell (tested on windows 10)
+* node
+
+## What?
+* Clears out previously generated certs from the store. (--noclear option)
+* Generates a rsa root cert and rsa cert signed by the root.
+* Deletes the generated rsa rootCA private key
+* Adds the rsa root cert (no private key) to the current users root certificate store.
+* Adds the rsa cert to the current users personal store.
+* Writes the rsa cert and key files to the current directory
+
+## Why?
+This is only made for local development testing when running a local web server.
+
+This is tested using the generated keys on a node web server.
+
+I would rather have developers do this than pass around an actual key signed by a real root CA.
+
+## How?
+install globally and run globally
+``` bash
+npm i -g win-cert-gen
+
+win-cert-gen -d local.example.com -e 720
+```
+
+## Arguments
+| Argument | Description | Default |
+| -------- |:-----------:|:-------:|
+| -d, --dns _domain_  | dns host for cert       | localhost |
+| -c, --cert _file_ | output file for the cert  | ./server.cert |
+| -k, --key _file_  | output file for the key   | ./server.key |
+| -e, --exp _hours_ | expiration in hours       | 24 |
+| -x, --noclear       | do not clear out previous generated certs |  |
---
+++ index.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+'use strict'
+const program = require('commander')
+const Shell = require('node-powershell')
+const fs = require('fs')
+const forge = require('node-forge')
+function uuidv4() { return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) { var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8); return v.toString(16) }) }
+// Its possible that we generate keys from node-forge and then import into key store
+program.option('-d, --dns <host>', 'dns host for cert (\'some.domain.com\')', 'localhost')
+program.option('-c, --cert <file>', 'output file for the cert ("./server.cert")', './server.cert')
+program.option('-k, --key <file>', 'output file for the key ("./server.key")', './server.key')
+program.option('-e, --exp <hours>', 'expiration in hours (24)', 24)
+program.option('-x, --noclear', 'do not clear out previous generated certs')
+// Add a deletion option to help clear out old generated certs
+program.password = uuidv4()
+program.parse(process.argv)
+if(!program.dns) {console.log('error: required option \'-d, --dns <host>\' not specified');program.outputHelp();process.exit(1)}
+if(typeof program.exp !== 'number') program.exp = parseInt(program.exp) || 24
+function BuildCommand(cmd, props) {function AddParameter(cmd, key, value) {cmd = cmd + ` -${key} ${value}`;return cmd} if(cmd === undefined || cmd === null) throw new Error('cmd must be defined'); if(typeof(props) !== 'object' || props.constructor !== Object) props = {}; let result = `${cmd}`;for (const key in props) if(props[key].constructor === Array) result = AddParameter(result, key, props[key].join(',')); else result = AddParameter(result, key, props[key]); return result }
+const friendlyName = 'win-cert-gen generated'
+function init(){
+  const ps2 = new Shell({executionPolicy: 'Bypass'})
+  if(!program.noclear) {
+    ps2.addCommand('$store = new-object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::Root,"currentuser")')
+    ps2.addCommand('$x=$store.Open("MaxAllowed")')
+    if(!program.noclear) ps2.addCommand(`$x = $store.Certificates.where({$_.FriendlyName -eq '${friendlyName}'},'Default').foreach{$store.remove($_)}`)
+    ps2.addCommand('$x=$store.Close();$x=$store.Dispose()')
+    ps2.addCommand('$store = new-object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::My,"currentuser")')
+    ps2.addCommand('$x=$store.Open("MaxAllowed")')
+    if(!program.noclear) ps2.addCommand(`$x = $store.Certificates.where({$_.FriendlyName -eq '${friendlyName}'},'Default').foreach{$store.remove($_)}`)
+    ps2.addCommand('$x=$store.Close();$x=$store.Dispose()')
+  }
+  ps2.addCommand(BuildCommand('$rootCA = New-SelfSignedCertificate', {
+    DnsName: `"${program.dns} Root Cert"`,
+    KeyLength: 2048,
+    KeyAlgorithm: 'RSA',
+    HashAlgorithm : 'SHA256',
+    KeyExportPolicy: 'NonExportable',
+    NotAfter: `(Get-Date).AddHours(${program.exp})`,
+    CertStoreLocation: 'Cert:\\CurrentUser\\My',
+    KeyUsage: ['CertSign', 'CRLSign'],
+    FriendlyName: `"${friendlyName}"`
+  }))
+  ps2.addCommand(BuildCommand('$selfCert = New-SelfSignedCertificate', {
+    DnsName: `"${program.dns}"`,
+    Signer: '$rootCA',
+    KeyLength: 2048,
+    KeyAlgorithm: 'RSA',
+    HashAlgorithm : 'SHA256',
+    KeyExportPolicy: 'Exportable',
+    NotAfter: `(Get-Date).AddHours(${program.exp})`,
+    CertStoreLocation: 'Cert:\\CurrentUser\\My',
+    FriendlyName: `"${friendlyName}"`
+  }))
+  ps2.addCommand('$rootCert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::New($rootCA.Export(1))')
+  ps2.addCommand(`$rootCert.FriendlyName = "${friendlyName}"`)
+  ps2.addCommand('$store = new-object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::Root,"currentuser")')
+  ps2.addCommand('$x=$store.Open("MaxAllowed");$x=$store.Add($rootCert);$x=$store.Close();$x = $store.Dispose()')
+  ps2.addCommand('$store = new-object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::My,"currentuser")')
+  ps2.addCommand('$x=$store.Open("MaxAllowed");$x=$store.Remove($rootCA);$x=$store.Close();$x = $store.Dispose()')
+  ps2.addCommand('$outCert = "-----BEGIN CERTIFICATE-----`r`n$([Convert]::ToBase64String($selfCert.Export(1), 0))`r`n-----END CERTIFICATE-----" -replace "(.{64})","`$1`r`n" -replace "(\\r\\n\\r\\n)","`r`n"')
+  ps2.addCommand('$outKey = "$([Convert]::ToBase64String($selfCert.Export(3, "'+program.password+'"), 0))"')
+  ps2.addCommand('$result = [PSCustomObject]@{cert=$outCert; pfx=$outKey}')
+  ps2.addCommand('$x=$selfCert.Reset();$x=$rootCA.Reset();$x=$rootCert.Reset()')
+  ps2.addCommand('$result | ConvertTo-Json -Compress')
+  ps2.invoke()
+    .then(output => {
+      try{
+        let result = JSON.parse(output)
+        if(!result.cert || !result.pfx) throw new Error('failed to export cert')
+        fs.writeFileSync(program.cert, result.cert)
+        let p12Raw = forge.pkcs12.pkcs12FromAsn1(forge.asn1.fromDer(forge.util.decode64(result.pfx)), program.password || '')
+        let oidShroudedPrivateKeyBag = '1.2.840.113549.1.12.10.1.2'
+        let key = forge.pki.privateKeyToPem(p12Raw.getBags({ bagType: oidShroudedPrivateKeyBag })[oidShroudedPrivateKeyBag][0].key)
+        fs.writeFileSync(program.key, key)
+        ps2.dispose()
+        console.log(`Successfully wrote cert to ${program.cert} and key to ${program.key}`)
+        process.exit()
+      } catch(error) {console.log(error);ps2.dispose();process.exit(1)}
+    })
+    .catch(err => {console.log(err);ps2.dispose();process.exit(1)})
+}
+init()
